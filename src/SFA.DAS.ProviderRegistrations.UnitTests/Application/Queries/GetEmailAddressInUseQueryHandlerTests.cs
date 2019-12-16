caeee95ce24b5fafd838ce1191bc0297db22dbe7
codewolf86 <chris@codewolfconsulting.com>
2019-12-16 08:47:48 +0000

    Tweaked GetEmailAddressInUse Unit Tests

--- src/SFA.DAS.ProviderRegistrations.UnitTests/Application/Queries/GetEmailAddressInUseQueryHandlerTests.cs
+++ src/SFA.DAS.ProviderRegistrations.UnitTests/Application/Queries/GetEmailAddressInUseQueryHandlerTests.cs
@@ -4,6 +4,9 @@ using SFA.DAS.ProviderRegistrations.UnitTests.AutoFixture;
 using System.Threading;
 using System.Threading.Tasks;
 using SFA.DAS.ProviderRegistrations.Application.Queries.GetEmailAddressInUseQuery;
+using AutoFixture.NUnit3;
+using Moq;
+using SFA.DAS.ProviderRegistrations.Services;
 
 namespace SFA.DAS.ProviderRegistrations.UnitTests.Application.Queries
 {
@@ -14,16 +17,35 @@ namespace SFA.DAS.ProviderRegistrations.UnitTests.Application.Queries
         [Test, ProviderAutoData]
         public async Task Handle_WhenHandlingGetEmailAddressInUseQueryHandlerAndParametersAreMatched_ThenShouldReturnATrueResult(
             string emailAddress,
+            [Frozen] Mock<IEmployerUsersService> employerUserService,
             GetEmailAddressInUseQueryHandler handler)
         {
             //arrange
             var query = new GetEmailAddressInUseQuery(emailAddress);
+            employerUserService.Setup(s => s.IsEmailAddressInUse(emailAddress, It.IsAny<CancellationToken>())).ReturnsAsync(true);
 
             //act
             var result = await handler.Handle(query, new CancellationToken());
 
             //assert
             result.Should().BeTrue();
+        }
+
+        [Test, ProviderAutoData]
+        public async Task Handle_WhenHandlingGetEmailAddressInUseQueryHandlerAndParametersAreNotMatched_ThenShouldReturnAFalseResult(
+            string emailAddress,
+            [Frozen] Mock<IEmployerUsersService> employerUserService,
+            GetEmailAddressInUseQueryHandler handler)
+        {
+            //arrange
+            var query = new GetEmailAddressInUseQuery(emailAddress);
+            employerUserService.Setup(s => s.IsEmailAddressInUse(emailAddress, It.IsAny<CancellationToken>())).ReturnsAsync(false);
+
+            //act
+            var result = await handler.Handle(query, new CancellationToken());
+
+            //assert
+            result.Should().BeFalse();
         }
     }
 }
