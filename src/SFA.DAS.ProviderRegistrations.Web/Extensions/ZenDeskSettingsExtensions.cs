e6cc71aca54d27f0d941084c9ca31e267702edf6
Paul Howes <paul.howes@cognetics.net>
2020-09-07 09:01:18 +0100

    ZenDesk widget

---
+++ src/SFA.DAS.ProviderRegistrations.Web/Extensions/ZenDeskSettingsExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using SFA.DAS.Provider.Shared.UI.Models;
+using SFA.DAS.Provider.Shared.UI.Startup;
+using SFA.DAS.ProviderRegistrations.Configuration;
+
+namespace SFA.DAS.ProviderRegistrations.Web.Extensions
+{
+    public static class ZenDeskExtensions
+    {
+        public static IMvcBuilder AddZenDeskSettings(this IMvcBuilder builder, IConfiguration config)
+        {
+            var zenDeskConfiguration = config.GetSection(ProviderRegistrationsConfigurationKeys.ZenDeskSettings).Get<ZenDeskConfiguration>();
+            builder.SetZenDeskConfiguration(zenDeskConfiguration);
+
+            return builder;
+        }
+    }
+}
