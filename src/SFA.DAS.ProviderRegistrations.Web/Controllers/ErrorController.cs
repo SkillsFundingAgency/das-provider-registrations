e907218ce8cc8a2b4f7da0e892b37b0cd6f24325
Balaji Jambulingam <balaji.jambulingam@digital.education.gov.uk>
2023-08-29 12:42:16 +0100

    Commit Details: 403 Forbidden Error Page with content relevant to DfESignIn
    https://skillsfundingagency.atlassian.net/browse/FAI-906

--- src/SFA.DAS.ProviderRegistrations.Web/Controllers/ErrorController.cs
+++ src/SFA.DAS.ProviderRegistrations.Web/Controllers/ErrorController.cs
@@ -1,6 +1,9 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using SFA.DAS.Provider.Shared.UI.Attributes;
+using SFA.DAS.ProviderRegistrations.Web.ViewModels;
+using System;
 
 namespace SFA.DAS.ProviderRegistrations.Web.Controllers
 {
@@ -8,12 +11,26 @@ namespace SFA.DAS.ProviderRegistrations.Web.Controllers
     [HideNavigationBar(hideAccountHeader: false, hideNavigationLinks: true)]
     public class ErrorController : Controller
     {
+        private readonly IConfiguration _configuration;
+
+        public ErrorController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+
         [Route("error/{statuscode?}")]
         public IActionResult Error(int? statusCode)
         {
+            var useDfESignIn = _configuration["UseDfESignIn"] != null && _configuration["UseDfESignIn"]
+                .Equals("true", StringComparison.CurrentCultureIgnoreCase);
+
             switch (statusCode)
             {
                 case 403:
+                    return View("403", new Error403ViewModel(_configuration["ResourceEnvironmentName"])
+                    {
+                        UseDfESignIn = useDfESignIn,
+                    });
                 case 404:
                     return View(statusCode.ToString());
                 default:
