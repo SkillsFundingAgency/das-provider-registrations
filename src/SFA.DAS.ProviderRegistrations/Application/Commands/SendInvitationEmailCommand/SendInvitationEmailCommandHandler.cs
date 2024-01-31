c209a65b436de93e6b512e55530ca1bdff7f20f6
Jon Harris <jon.harris@digital.education.gov.uk>
2024-01-31 12:10:20 +0000

    Making handler async

--- src/SFA.DAS.ProviderRegistrations/Application/Commands/SendInvitationEmailCommand/SendInvitationEmailCommandHandler.cs
+++ src/SFA.DAS.ProviderRegistrations/Application/Commands/SendInvitationEmailCommand/SendInvitationEmailCommandHandler.cs
@@ -9,20 +9,21 @@ public class SendInvitationEmailCommandHandler : IRequestHandler<SendInvitationE
     private readonly string _notificationTemplateId = "ProviderInviteEmployerNotification";
     private readonly IMessageSession _publisher;
     private readonly ProviderRegistrationsSettings _configuration;
-    
+
     public SendInvitationEmailCommandHandler(IMessageSession publisher, ProviderRegistrationsSettings configuration)
     {
         _publisher = publisher;
         _configuration = configuration;
-        
+
         if (_configuration.UseGovLogin)
         {
-            _notificationTemplateId = _configuration.ResourceEnvironmentName.ToLower() == "prd" 
-                ? "9dc52d84-0ee5-4755-b836-f4e71ae2a326" : "02818d7b-cea1-4445-8b16-5a27f40ddaf6";
+            _notificationTemplateId = _configuration.ResourceEnvironmentName.ToLower() == "prd"
+                ? "9dc52d84-0ee5-4755-b836-f4e71ae2a326"
+                : "02818d7b-cea1-4445-8b16-5a27f40ddaf6";
         }
     }
 
-    public Task Handle(SendInvitationEmailCommand request, CancellationToken cancellationToken)
+    public async Task Handle(SendInvitationEmailCommand request, CancellationToken cancellationToken)
     {
         var tokens = new Dictionary<string, string>
         {
@@ -34,9 +35,7 @@ public class SendInvitationEmailCommandHandler : IRequestHandler<SendInvitationE
             { "unsubscribe_training_provider", $"{_configuration.EmployerAccountsBaseUrl}/service/unsubscribe/{request.CorrelationId}" },
             { "report_training_provider", $"{_configuration.EmployerAccountsBaseUrl}/report/trainingprovider/{request.CorrelationId}" }
         };
-        
-        _publisher.Send(new SendEmailCommand(_notificationTemplateId, request.EmployerEmail, tokens));
 
-        return Task.CompletedTask;
+        await _publisher.Send(new SendEmailCommand(_notificationTemplateId, request.EmployerEmail, tokens));
     }
 }
