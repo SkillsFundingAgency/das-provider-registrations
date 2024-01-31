f015ed46a0268f71c0a55c1a8e8e575cce60f809
Jon Harris <jon.harris@digital.education.gov.uk>
2024-01-31 15:52:07 +0000

    Tidy

--- src/SFA.DAS.ProviderRegistrations.MessageHandlers/Extensions/ServiceCollectionExtensions.cs
+++ src/SFA.DAS.ProviderRegistrations.MessageHandlers/Extensions/ServiceCollectionExtensions.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Azure.ServiceBus.Primitives;
+﻿using System.Reflection;
+using Microsoft.Azure.ServiceBus.Primitives;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
