9f8d692cde1ba18c0abe4c9bee6c45382c010ea2
Jon Harris <jon.harris@digital.education.gov.uk>
2024-02-28 11:12:35 +0000

    Implementing SFA.DAS.EmailValidationService package to validate email addresses

--- src/SFA.DAS.ProviderRegistrations.Web.UnitTests/Validation/MailAddressAttributeTests.cs
+++ src/SFA.DAS.ProviderRegistrations.Web.UnitTests/Validation/MailAddressAttributeTests.cs
@@ -1,47 +1,22 @@
-﻿using NUnit.Framework;
+﻿using FluentAssertions;
+using NUnit.Framework;
 using SFA.DAS.ProviderRegistrations.Web.Validation;
 
-namespace SFA.DAS.ProviderRegistrations.Web.UnitTests.Validation
+namespace SFA.DAS.ProviderRegistrations.Web.UnitTests.Validation;
+
+public class EmailValidationTests
 {
-    [TestFixture]
-    class MailAddressAttributeTests
+    [TestCaseSource(typeof(ValidEmailCases))]
+    public void EmailShouldBeValid(string email)
     {
-        [TestCase("email@example.com", true)]
-        [TestCase("EmAiL@Example.COM", true)]
-        [TestCase("firstname.lastname @example.com", false)]
-        [TestCase("firstname.lastname@example.com", true)]
-        [TestCase("email @subdomain.example.com", false)]
-        [TestCase("firstname+lastname @example.com", false)]
-        [TestCase("firstname+lastname@example.com", true)]
-        [TestCase("email@123.123.123.123", true)]
-        [TestCase("email@[123.123.123.123]", true)]
-        [TestCase("\"email\"@example.com", true)]
-        [TestCase("1234567890@example.com", true)]
-        [TestCase("email@example-one.com", true)]
-        [TestCase("_______@example.com", true)]
-        [TestCase("email@example.name", true)]
-        [TestCase("em,ail@example.name", false)]
-        [TestCase("e;mail@example.name", false)]
-        [TestCase("email@example.museum", true)]
-        [TestCase("email@example.co.jp", true)]
-        [TestCase("firstname-lastname @example.com", false)]
-        [TestCase("plainaddress", false)]
-        [TestCase("#@%^%#$@#$@#.com", false)]
-        [TestCase("@example.com", false)]
-        [TestCase("Joe Smith <email @example.com>", false)]
-        [TestCase("email.example.com", false)]
-        [TestCase("email@example @example.com", false)]
-        [TestCase(".email @example.com", false)]
-        [TestCase("email.@example.com", true)]
-        [TestCase("email..email @example.com", false)]
-        [TestCase("email@example.com (Joe Smith)", false)]
-        [TestCase("email@-example.com", true)]
-        [TestCase("email@example.web", true)]
-        [TestCase("email @example..com", false)]
-        public void ValidateEmailAddress(string emailAddress, bool expectedResult)
-        {
-            var validator = new MailAddressAttribute();
-            Assert.That(validator.IsValid(emailAddress), Is.EqualTo(expectedResult));
-        }
+        var validator = new MailAddressAttribute();
+        validator.IsValid(email).Should().BeTrue();
+    }
+
+    [TestCaseSource(typeof(InvalidEmailCases))]
+    public void EmailShouldBeInvalid(string email)
+    {
+        var validator = new MailAddressAttribute();
+        validator.IsValid(email).Should().BeFalse();
     }
 }
